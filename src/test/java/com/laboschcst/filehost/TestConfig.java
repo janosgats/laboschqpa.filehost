da89dfaeccf7295c8c48aea10297652e190cf5a8
Dzsani <gatsjanos@gmail.com>
2020-03-08 17:02:16 +0100

    Fixing test.

--- src/test/java/com/laboschcst/filehost/TestConfig.java
+++ src/test/java/com/laboschcst/filehost/TestConfig.java
@@ -5,7 +5,6 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 
 @TestConfiguration
@@ -19,10 +18,5 @@ public class TestConfig {
     @Bean
     public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();//Returning an empty bean
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return Mockito.mock(ClientRegistrationRepository.class);
     }
 }
