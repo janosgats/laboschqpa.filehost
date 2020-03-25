926c3e2261712e7d19f3b466cb9f664d0b73294a
Dzsani <gatsjanos@gmail.com>
2020-03-25 04:23:48 +0100

    Removing unused dependency.

--- src/test/java/com/laboschcst/filehost/TestConfig.java
+++ src/test/java/com/laboschcst/filehost/TestConfig.java
@@ -1,7 +1,5 @@
 package com.laboschcst.filehost;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -9,12 +7,6 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @TestConfiguration
 public class TestConfig {
-
-    @Bean
-    public HikariDataSource dataSource() {
-        return Mockito.mock(HikariDataSource.class);
-    }
-
     @Bean
     public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();//Returning an empty bean
