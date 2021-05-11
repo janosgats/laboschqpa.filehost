3816c41ea6c06afeb959465f3e93bb39feaaef45
Dzsani <gatsjanos@gmail.com>
2021-05-11 22:09:31 +0200

    StreamBenchmarkFilter enhancements

--- src/main/java/com/laboschqpa/filehost/config/filter/StreamBenchmarkFilter.java
+++ src/main/java/com/laboschqpa/filehost/config/filter/StreamBenchmarkFilter.java
@@ -4,8 +4,9 @@ import com.laboschqpa.filehost.model.inputstream.TrackingInputStream;
 import com.laboschqpa.filehost.model.streamtracking.TrackingInputStreamFactory;
 import com.laboschqpa.filehost.util.ServletHelper;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.log4j.Log4j2;
+import org.apache.tomcat.util.http.fileupload.FileItemIterator;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -14,13 +15,15 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
+@Log4j2
 @Component
 @Order(2)
 @RequiredArgsConstructor
 public class StreamBenchmarkFilter implements Filter {
-    private static final Logger logger = LoggerFactory.getLogger(StreamBenchmarkFilter.class);
+    private static final int EOF = -1;
 
     private final TrackingInputStreamFactory trackingInputStreamFactory;
 
@@ -38,28 +41,47 @@ public class StreamBenchmarkFilter implements Filter {
         }
     }
 
-    private void doBenchmark(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
-        logger.warn("Doing stream benchmark on URL {}", httpServletRequest.getRequestURI());
-        TrackingInputStream trackingInputStream = trackingInputStreamFactory.createForFileUpload(httpServletRequest.getInputStream());
+    private void doBenchmark(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+        log.warn("Doing stream benchmark on URL {}", httpServletRequest.getRequestURI());
+
+
+        final ServletFileUpload servletFileUpload = new ServletFileUpload();
+
+        FileItemIterator iterator = servletFileUpload.getItemIterator(httpServletRequest);
+
+        byte[] readBuff = new byte[2000000];
 
         long readStartTime = System.nanoTime();
-        byte[] readBuff = new byte[2000000];
-        while (trackingInputStream.read(readBuff) != 0) ;
+
+        long countOfReadBytes = 0;
+        while (iterator.hasNext()) {
+            final InputStream partInputStream = iterator.next().openStream();
+            final TrackingInputStream trackingInputStream = trackingInputStreamFactory.createForFileUpload(partInputStream);
+
+            while (trackingInputStream.read(readBuff) != EOF) ;
+
+            trackingInputStream.close();
+            countOfReadBytes += trackingInputStream.getCountOfReadBytes();
+        }
+
         long readEndTime = System.nanoTime();
 
-        trackingInputStream.close();
 
-        long readTimeNano = readEndTime - readStartTime;
+        double countOfReadMegaBytes = countOfReadBytes / (double) (1000 * 1000);
 
-        ServletHelper.setJsonResponse(httpServletResponse, Map.of(
+        long readTimeNanos = readEndTime - readStartTime;
+        double readTimeSeconds = readTimeNanos / (double) (1000 * 1000 * 1000);
+
+        Map<String, String> benchmarkResult = Map.of(
                 "result", "success",
-                "countOfReadBytes", trackingInputStream.getCountOfReadBytes(),
-                "countOfReadKBytes", trackingInputStream.getCountOfReadBytes() / (double) (1000),
-                "countOfReadMBytes", trackingInputStream.getCountOfReadBytes() / (double) (1000 * 1000),
-                "readTimeNanos", readTimeNano,
-                "readTimeMillis", readTimeNano / (double) (1000 * 1000),
-                "readTimeSeconds", readTimeNano / (double) (1000 * 1000 * 1000)
-        ), 200);
+                "countOfReadBytes", String.valueOf(countOfReadBytes),
+                "countOfReadMegaBytes", String.valueOf(countOfReadMegaBytes),
+                "readTimeNanos", String.valueOf(readTimeNanos),
+                "readTimeSeconds", String.valueOf(readTimeSeconds),
+                "speedMBPerSec", String.valueOf(countOfReadMegaBytes / readTimeSeconds)
+        );
+        log.info("Stream Benchmark Result: {}", benchmarkResult);
+        ServletHelper.setJsonResponse(httpServletResponse, benchmarkResult, 200);
     }
 
     @Value("${streambenchmarkfilter.enable:false}")
