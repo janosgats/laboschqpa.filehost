fa9e25b49454e72bac9b6d380bd8d4e5af37475f
Dzsani <gatsjanos@gmail.com>
2021-09-24 15:57:47 +0200

    Added listSucceededVariantIdsOfFile endpoint

--- src/main/java/com/laboschqpa/filehost/api/controller/internal/ImageVariantController.java
+++ src/main/java/com/laboschqpa/filehost/api/controller/internal/ImageVariantController.java
@@ -2,18 +2,19 @@ package com.laboschqpa.filehost.api.controller.internal;
 
 import com.laboschqpa.filehost.api.dto.FileUploadResponse;
 import com.laboschqpa.filehost.entity.IndexedFileEntity;
+import com.laboschqpa.filehost.enums.ImageVariantStatus;
+import com.laboschqpa.filehost.repo.ImageVariantRepository;
 import com.laboschqpa.filehost.service.imagevariant.VariantSaverService;
 import com.laboschqpa.filehost.service.imagevariant.VariantService;
 import com.laboschqpa.filehost.service.imagevariant.command.SaveImageVariantCommand;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -22,6 +23,7 @@ import javax.servlet.http.HttpServletRequest;
 public class ImageVariantController {
     private final VariantSaverService variantSaverService;
     private final VariantService variantService;
+    private final ImageVariantRepository imageVariantRepository;
 
     @ApiOperation("To upload the result of a variant creation job.")
     @PostMapping("/uploadVariant")
@@ -48,5 +50,12 @@ public class ImageVariantController {
     @PostMapping("/markVariantAsCorrupt")
     public void postMarkVariantAsCorrupt(@RequestParam("originalFileId") Long originalFileId, @RequestParam("variantSize") Integer variantSize) {
         variantService.markVariantAsCorrupt(originalFileId, variantSize);
+    }
+
+    @GetMapping("/listSucceededVariantIdsOfFile")
+    public List<Long> getListSucceededVariantIdsOfFile(@RequestParam("originalFileId") Long originalFileId) {
+        return imageVariantRepository.findAllByOriginalFileIdAndStatus(originalFileId, ImageVariantStatus.SUCCEEDED).stream()
+                .map(imageVariant -> imageVariant.getVariantFile().getId())
+                .collect(Collectors.toList());
     }
 }
